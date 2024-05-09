212cd22adc8918bc9029c3f3c9f4ffb476dd9908
Milton Pividori <miltondp@gmail.com>
2024-05-09 14:42:57 -0600

    scripts/env: update default configuration and paths

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,28 +10,30 @@
 
 # BASE_DIR is the parent directory where the code and manuscript repos are
 # located.
-BASE_DIR=/home/miltondp/projects/phenoplier/greenelab
+BASE_DIR=/home/miltondp/projects/phenoplier/clean_orig_base/phenoplier
 
 # Project name
-PROJECT_NAME=phenoplier
+#PROJECT_NAME=phenoplier
 
 # Number of CPUs to use
-export PHENOPLIER_N_JOBS=3
-
-# Docker image tag to use
-DOCKER_IMAGE_TAG="latest"
+export PHENOPLIER_N_JOBS=1
 
 #
 # Do not edit below
 #
-export PHENOPLIER_ROOT_DIR=${BASE_DIR}/${PROJECT_NAME}/base
+export PHENOPLIER_ROOT_DIR=${BASE_DIR}
 echo "PHENOPLIER_ROOT_DIR=${PHENOPLIER_ROOT_DIR}"
 
-export PHENOPLIER_MANUSCRIPT_DIR=${BASE_DIR}/${PROJECT_NAME}_manuscript/
+export PHENOPLIER_MANUSCRIPT_DIR=${BASE_DIR}/manuscript
 echo "PHENOPLIER_MANUSCRIPT_DIR=${PHENOPLIER_MANUSCRIPT_DIR}"
 
-export PHENOPLIER_DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG:-latest}"
-echo "PHENOPLIER_DOCKER_IMAGE_TAG=${PHENOPLIER_DOCKER_IMAGE_TAG}"
+export PHENOPLIER_CODE_DIR=/home/miltondp/projects/phenoplier/phenoplier-gls-nulls
+echo "PHENOPLIER_CODE_DIR=${PHENOPLIER_CODE_DIR}"
 
-export PYTHONPATH=${BASE_DIR}/${PROJECT_NAME}/libs/:${PYTHONPATH}
+export PYTHONPATH=${PHENOPLIER_CODE_DIR}/libs/:${PYTHONPATH}
 echo "PYTHONPATH=${PYTHONPATH}"
+
+# GTEx v8
+export PHENOPLIER_GTEX_V8_DIR="/home/miltondp/projects/phenoplier/clean_orig_base/gtex_v8/"
+echo "PHENOPLIER_GTEX_V8_DIR=${PHENOPLIER_GTEX_V8_DIR}"
+
