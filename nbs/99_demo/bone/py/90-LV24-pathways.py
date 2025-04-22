2bc7e684ca6ee7d015815a7ef1bbd24a0b49a8b0
Milton Pividori <miltondp@gmail.com>
2025-04-22 13:37:28 -0600

    bone/LV24: update pathways notebook

--- nbs/99_demo/bone/py/90-LV24-pathways.py
+++ nbs/99_demo/bone/py/90-LV24-pathways.py
@@ -18,7 +18,9 @@
 # # Description
 
 # %% [markdown] tags=[]
-# Generates manubot tables for pathways enriched (from the MultiPLIER models) given an LV name (in Settings below).
+# It shows the pathways enriched (from the MultiPLIER models) given an LV name (in Settings below).
+# These "pathways enriched" are a set of limited pathways used during training of this PLIER model.
+# More pathways might be enriched if using external and more comprehensive databases such as gProfiler or FUMA as shown below.
 
 # %% [markdown] tags=[]
 # # Modules loading
@@ -42,14 +44,13 @@ import conf
 # %% tags=["parameters"]
 LV_NAME = "LV24"
 
-# %%
-# assert (
-#     conf.MANUSCRIPT["BASE_DIR"] is not None
-# ), "The manuscript directory was not configured"
+# %% [markdown] tags=[]
+# # Paths
 
-# OUTPUT_FILE_PATH = conf.MANUSCRIPT["CONTENT_DIR"] / "50.00.supplementary_material.md"
-# display(OUTPUT_FILE_PATH)
-# assert OUTPUT_FILE_PATH.exists()
+# %%
+OUTPUT_FIGURES_DIR = Path(conf.RESULTS_DIR, "demo", f"{LV_NAME.lower()}").resolve()
+display(OUTPUT_FIGURES_DIR)
+OUTPUT_FIGURES_DIR.mkdir(parents=True, exist_ok=True)
 
 # %% [markdown] tags=[]
 # # Load MultiPLIER summary
@@ -71,7 +72,7 @@ lv_pathways = multiplier_model_summary[
     multiplier_model_summary["LV index"].isin((LV_NAME[2:],))
     & (
         (multiplier_model_summary["FDR"] < 0.05)
-        #         | (multiplier_model_summary["AUC"] >= 0.75)
+                | (multiplier_model_summary["AUC"] >= 0.75)
     )
 ]
 
@@ -97,13 +98,62 @@ lv_pathways = lv_pathways.rename(
 # %%
 lv_pathways.head()
 
+# %% [markdown] tags=[]
+# # Load LV data
+
+# %%
+from data.recount2 import LVAnalysis
+
+# %%
+lv_obj = LVAnalysis(LV_NAME)
+
+# %% [markdown] tags=[]
+# Here I show the top 20 genes for our LV. You can see gene symbols, the LV weight (in column `LV603`) and the cytoband.
+
+# %%
+lv_obj.lv_genes.head(20)
+
 # %% [markdown]
-# ## Split names
+# # Pathway enrichment using external databases
+
+# %% [markdown]
+# ## gProfiler
 
 # %%
-lv_pathways["Pathway"] = lv_pathways["Pathway"].apply(lambda x: " ".join(x.split("_")))
+print(" ".join(lv_obj.lv_genes.head(70)["gene_name"].tolist()))
+
+# %% [markdown]
+# Copy/paste the list of genes above and use gProfiler: https://biit.cs.ut.ee/gprofiler/gost
+
+# %% [markdown]
+# Results URL: https://biit.cs.ut.ee/gplink/l/a5kY96GBjSk
+#
+# **Notes**: seems to be very skin related.
+
+# %% [markdown]
+# ## FUMA
 
 # %%
-lv_pathways.head()
+# print top genes in module
+print("\n".join(lv_obj.lv_genes.head(70)["gene_name"].tolist()))
+
+# %%
+# save all genes in model to use as background list of genes
+lv_obj.lv_genes["gene_name"].to_csv(OUTPUT_FIGURES_DIR / "all_genes.txt", header=None, index=False)
+
+# %%
+OUTPUT_FIGURES_DIR / "all_genes.txt"
+
+# %%
+# !head /opt/data/results/demo/lv24/all_genes.txt
+
+# %%
+# !wc -l /opt/data/results/demo/lv24/all_genes.txt
+
+# %% [markdown]
+# Now go to the FUMA GENE2FUNC module here: https://fuma.ctglab.nl/gene2func
+#
+# 1. Paste the list of top genes above and then upload the `all_genes.txt` file.
+# 2. Use a "Title" and click on "Submit"
 
 # %%
